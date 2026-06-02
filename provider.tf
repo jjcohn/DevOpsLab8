c61a7f6c6b2c656be0294b4ac9e81c30eee979f9
jacob <jacob@none.com>
2026-06-02 14:21:38 +0000

    update provider

--- provider.tf
+++ provider.tf
@@ -16,8 +16,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  client_id       = "<Service Principle ID>"
-  client_secret   = "<Service Principle Secret>"
-  tenant_id       = "<tenant_ID>"
-  subscription_id = "<Subscription_ID>"
+  client_id       = "299bd8ff-eec3-4110-a9d3-7fd4f44d502f"
+  client_secret   = "r3r8Q~s_KMv1nTCEFyi2eyuDWGbqaF44cw32-c_Z"
+  tenant_id       = "bb7ed293-2674-4aef-a74a-dbf340a8ab33"
+  subscription_id = "9a26f90a-cc5b-48a0-aabf-4b7fe4678c68"
 }
